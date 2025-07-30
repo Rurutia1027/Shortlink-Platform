894adaa08f37c60d3c99a7cde59d39f0467d4e92
Rurutia1027 <rurutia1027@gmail.com>
2025-07-30 17:22:53 +0800

    declare redis distributed lock name for user registry

---
+++ admin/src/main/java/org/ucd/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -0,0 +1,11 @@
+package org.ucd.shortlink.admin.common.constant;
+
+/**
+ * short link platform redis cache constants
+ */
+public class RedisCacheConstant {
+    /**
+     * User register distributed lock
+     */
+    public static final String LOCK_USER_REGISTER_KEY = "short-link:lock_user-register:";
+}
