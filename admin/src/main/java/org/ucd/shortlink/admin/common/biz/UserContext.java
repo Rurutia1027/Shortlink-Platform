b161c197ce3603ad43f218caa12464950da19837
Rurutia1027 <rurutia1027@gmail.com>
2025-07-31 20:12:05 +0800

    feature: support user login context info intercept and store to thread local context

--- admin/src/main/java/org/ucd/shortlink/admin/common/biz/UserContext.java
+++ admin/src/main/java/org/ucd/shortlink/admin/common/biz/UserContext.java
@@ -1,4 +1,58 @@
 package org.ucd.shortlink.admin.common.biz;
 
+import com.alibaba.ttl.TransmittableThreadLocal;
+
+import java.util.Optional;
+
+/**
+ * User Context
+ */
 public class UserContext {
+    private static final ThreadLocal<UserInfoDTO> USER_THREAD_LOCAL = new TransmittableThreadLocal<>();
+
+    /**
+     * Set user info to UserContext
+     *
+     * @param user context info
+     */
+    public static void setUser(UserInfoDTO user) {
+        USER_THREAD_LOCAL.set(user);
+    }
+
+    /**
+     * Fetch user ID from UserContext
+     *
+     * @return User ID
+     */
+    public static String getUserId() {
+        UserInfoDTO userInfoDTO = USER_THREAD_LOCAL.get();
+        return Optional.ofNullable(userInfoDTO).map(UserInfoDTO::getUserId).orElse(null);
+    }
+
+    /**
+     * Fetch username from UserContext
+     *
+     * @return username
+     */
+    public static String getUsername() {
+        UserInfoDTO userInfoDTO = USER_THREAD_LOCAL.get();
+        return Optional.of(userInfoDTO).map(UserInfoDTO::getUsername).orElse(null);
+    }
+
+    /**
+     * Fetch user real name from UserContext
+     *
+     * @return user real name
+     */
+    public static String getRealName() {
+        UserInfoDTO userInfoDTO = USER_THREAD_LOCAL.get();
+        return Optional.ofNullable(userInfoDTO).map(UserInfoDTO::getRealName).orElse(null);
+    }
+
+    /**
+     * Clean UserContext
+     */
+    public static void removeUser() {
+        USER_THREAD_LOCAL.remove();
+    }
 }
--- admin/src/main/java/org/ucd/shortlink/admin/common/biz/UserTransmitFilter.java
+++ admin/src/main/java/org/ucd/shortlink/admin/common/biz/UserTransmitFilter.java
@@ -1,4 +1,56 @@
 package org.ucd.shortlink.admin.common.biz;
 
-public class UserTransmitFilter {
+import cn.hutool.core.util.StrUtil;
+import com.alibaba.fastjson2.JSON;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.ucd.shortlink.admin.common.constant.RedisCacheConstant;
+
+import java.io.IOException;
+
+/**
+ * User info transmit filter to intercept user info from request chain and insert to
+ * {@link UserContext}
+ */
+@RequiredArgsConstructor
+public class UserTransmitFilter implements Filter {
+    private final StringRedisTemplate stringRedisTemplate;
+
+    @SneakyThrows
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        String username = httpServletRequest.getHeader("username");
+        String token = httpServletRequest.getHeader("token");
+        Object userInfoJsonStr =
+                stringRedisTemplate.opsForHash().get(RedisCacheConstant.USER_LOGIN_KEY + username,
+                        token);
+        if (userInfoJsonStr != null) {
+            UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(),
+                    UserInfoDTO.class);
+            UserContext.setUser(userInfoDTO);
+        }
+
+        if (StrUtil.isNotBlank(username)) {
+            String userId = httpServletRequest.getHeader("userId");
+            String realName = httpServletRequest.getHeader("realName");
+            UserInfoDTO userInfoDTO = new UserInfoDTO(userId, username, realName);
+            UserContext.setUser(userInfoDTO);
+        }
+
+        try {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } finally {
+            UserContext.removeUser();
+        }
+
+    }
 }
