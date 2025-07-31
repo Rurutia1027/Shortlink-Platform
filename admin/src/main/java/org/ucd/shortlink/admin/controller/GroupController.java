0aef02f2a28097c3720392bffe8fa5e852f2dd48
Rurutia1027 <rurutia1027@gmail.com>
2025-07-31 19:18:09 +0800

    feature: support group sorting

--- admin/src/main/java/org/ucd/shortlink/admin/controller/GroupController.java
+++ admin/src/main/java/org/ucd/shortlink/admin/controller/GroupController.java
@@ -1,13 +1,17 @@
 package org.ucd.shortlink.admin.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.ucd.shortlink.admin.common.convention.result.Result;
 import org.ucd.shortlink.admin.common.convention.result.Results;
 import org.ucd.shortlink.admin.dto.req.ShortLinkGroupSaveReqDTO;
+import org.ucd.shortlink.admin.dto.resp.ShortLinkGroupRespDTO;
 import org.ucd.shortlink.admin.service.GroupService;
+
+import java.util.List;
 
 /**
  * short linke grouping control layer
@@ -17,9 +21,21 @@ import org.ucd.shortlink.admin.service.GroupService;
 public class GroupController {
     private final GroupService groupService;
 
+    /**
+     * Add new short link group item
+     */
     @PostMapping("/api/short-link/v1/group")
     Result<Void> save(@RequestBody ShortLinkGroupSaveReqDTO requestParam) {
         groupService.saveGroup(requestParam.getName());
         return Results.success();
     }
+
+    /**
+     *
+     */
+    @GetMapping("/api/short-link/v1/group")
+    public Result<List<ShortLinkGroupRespDTO>> listGroup() {
+        return Results.success( groupService.listGroup());
+    }
+
 }
