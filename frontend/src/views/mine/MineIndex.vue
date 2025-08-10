546ad59f9af688029dcde5c10cbf38804158c3da
Rurutia1027 <rurutia1027@gmail.com>
2025-08-10 18:19:01 +0800

    feat: add other views

---
+++ frontend/src/views/mine/MineIndex.vue
@@ -0,0 +1,157 @@
+<template>
+  <div style="display: flex; height: 100%; width: 100%;">
+    <div class="options-box">
+      <div>
+        <span>Account Settings</span>
+      </div>
+    </div>
+    <div class="main-box">
+      <el-descriptions class="margin-top content-box" title="Personal Information" :column="1" :size="size" border>
+        <template #extra>
+        </template>
+        <el-descriptions-item>
+          <template #label>
+            <div class="cell-item">
+              <el-icon :style="iconStyle">
+                <user/>
+              </el-icon>
+              Username
+            </div>
+          </template>
+          <span v-if="!dialogVisible">{{ userInfo?.data?.data?.username }}</span>
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template #label>
+            <div class="cell-item">
+              <el-icon :style="iconStyle">
+                <iphone/>
+              </el-icon>
+              Phone Number
+            </div>
+          </template>
+          <span>{{ userInfo?.data?.data?.phone }}</span>
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template #label>
+            <div class="cell-item">
+              <el-icon :style="iconStyle">
+                <tickets/>
+              </el-icon>
+              Full Name
+            </div>
+          </template>
+          <span>{{ userInfo?.data?.data?.realName }}</span>
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template #label>
+            <div class="cell-item">
+              <el-icon :style="iconStyle">
+                <Message/>
+              </el-icon>
+              Email
+            </div>
+          </template>
+          <span>{{ userInfo?.data?.data?.mail }}</span>
+        </el-descriptions-item>
+      </el-descriptions>
+      <el-button type="primary" @click="dialogVisible = !dialogVisible">Edit Personal Information</el-button>
+    </div>
+  </div>
+  <!-- Edit Information -->
+  <el-dialog v-model="dialogVisible" title="Edit Personal Information" width="60%" :before-close="handleClose">
+    <div class="register" :class="{ hidden: isLogin }">
+      <el-form ref="loginFormRef" :model="userInfoForm" label-width="50px" class="form-container" width="width"
+               :rules="formRule">
+        <el-form-item prop="username">
+          <el-input v-model="userInfoForm.username" placeholder="Please enter username" maxlength="11" show-word-limit
+                    disabled>
+            <template v-slot:prepend> Username</template>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="mail">
+          <el-input v-model="userInfoForm.mail" placeholder="Please enter email" show-word-limit clearable>
+            <template v-slot:prepend> E<span class="second-font">mail</span></template>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="phone">
+          <el-input v-model="userInfoForm.phone" placeholder="Please enter phone number" show-word-limit clearable>
+            <template v-slot:prepend> Phone Number</template>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="realName">
+          <el-input v-model="userInfoForm.realName" placeholder="Please enter full name" show-word-limit clearable>
+            <template v-slot:prepend> F<span class="second-font">ull Name</span></template>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input v-model="userInfoForm.password"
+                    placeholder="Default password, enter new password if you want to change" show-word-limit clearable>
+            <template v-slot:prepend> P<span class="second-font">assword</span></template>
+          </el-input>
+        </el-form-item>
+        <el-form-item>
+          <div style=" width: 100%;display: flex; justify-content: flex-end;">
+            <el-button @click="dialogVisible = false">Cancel</el-button>
+            <el-button type="primary" @click="changeUserInfo(loginFormRef)">
+              Submit
+            </el-button>
+          </div>
+        </el-form-item>
+      </el-form>
+    </div>
+  </el-dialog>
+</template>
+
+<script setup>
+// (No Chinese comments here)
+</script>
+
+<style lang="scss" scoped>
+.main-box {
+  flex: 1;
+  padding: 15px;
+  background-color: rgb(238, 240, 245);
+  height: calc(100vh - 50px);
+  display: flex;
+  flex-direction: column;
+}
+
+.content-box {
+  flex: 1;
+  background-color: white;
+  padding: 20px;
+}
+
+.register {
+  padding-right: 30px;
+}
+
+:deep(.el-descriptions__label) {
+  width: 200px !important;
+}
+
+.second-font {
+  margin-left: 13px;
+}
+
+.options-box {
+  position: relative;
+  height: 100%;
+  width: 190px;
+  border-right: 1px solid rgba(0, 0, 0, 0.1);
+  display: flex;
+  padding-top: 15px;
+
+  div {
+    flex: 1;
+    display: flex;
+    height: 50px;
+    align-items: center;
+    justify-content: center;
+    background-color: rgb(235, 239, 250);
+    font-family: PingFangSC-Semibold, PingFang SC;
+    color: #3464e0;
+    font-weight: 600;
+  }
+}
+</style>
---
+++ frontend/src/views/mySpace/MySpaceIndex.vue
@@ -0,0 +1,318 @@
+
+<style lang="scss" scoped>
+.flex-box {
+  display: flex;
+  align-items: center;
+  padding: 0 10px;
+  justify-content: space-between;
+}
+
+.hover-box:hover {
+  cursor: pointer;
+  color: rgba(40, 145, 206, 0.6);
+  background-color: #f7f7f7;
+  box-shadow: 0px 2px 8px 0px rgba(28, 41, 90, 0.1);
+}
+
+.option-title {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 56px;
+  font-size: 15px;
+  font-weight: 600;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+
+  span {
+    font-size: 12px;
+    font-weight: 400;
+  }
+}
+
+.options-box {
+  position: relative;
+  height: 100%;
+  width: 190px;
+  border-right: 1px solid rgba(0, 0, 0, 0.1);
+
+  .item-box {
+    height: 43px;
+    width: 190px;
+    font-family:
+        PingFangSC-Semibold,
+        PingFang SC;
+    font-weight: 520;
+  }
+
+  .item-box:hover {
+    .flex-box {
+      .edit {
+        display: block;
+      }
+
+      .item-length {
+        display: none !important;
+      }
+    }
+  }
+}
+
+.recycle-bin {
+  position: absolute;
+  display: flex;
+  bottom: 0;
+  height: 50px;
+  width: 100%;
+}
+
+.recycle-box {
+  flex: 1;
+  border-top: 1px solid rgba(0, 0, 0, 0.1);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.edit {
+  display: none;
+  margin-left: 5px;
+  color: rgb(83, 97, 97);
+  font-size: 20px;
+}
+
+.edit:hover {
+  color: #2991ce;
+  cursor: pointer;
+}
+
+.zero {
+  color: rgb(83, 97, 97) !important;
+}
+
+// 提示框样式
+.tooltip-base-box {
+  width: 600px;
+}
+
+.tooltip-base-box .row {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.tooltip-base-box .center {
+  justify-content: center;
+}
+
+.tooltip-base-box .box-item {
+  width: 110px;
+}
+
+.selectedItem {
+  color: #3464e0 !important;
+  background-color: #ebeffa !important;
+  font-weight: 600 !important;
+}
+
+.block:hover {
+  .el-icon {
+    color: rgb(121, 187, 255) !important;
+  }
+}
+
+.table-edit {
+  font-size: 20px;
+  margin-right: 20px;
+  color: #3677c2;
+  cursor: pointer;
+}
+
+.table-edit:hover {
+  color: #98cafe;
+}
+
+.qr-code {
+  margin-right: 20px;
+  cursor: pointer;
+}
+
+.qr-code:hover {
+  opacity: 0.5;
+}
+
+.content-box {
+  flex: 1;
+  padding: 16px;
+  background-color: #eef0f5;
+  position: relative;
+
+  .table-box {
+    background-color: #ffffff;
+    height: 100%;
+
+    .buttons-box {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: 16px;
+    }
+
+    .pagination-block {
+      position: absolute;
+      bottom: 4%;
+      left: 50%;
+      transform: translate(-50%, 0);
+    }
+
+    .recycle-bin-box {
+      height: 64px;
+      display: flex;
+      align-items: center;
+      padding-left: 16px;
+
+      span:nth-child(1) {
+        font-size: 20px;
+        margin-right: 5px;
+      }
+    }
+  }
+}
+
+.over-text {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 1;
+}
+
+.table-link-box {
+  display: flex;
+  align-items: center;
+
+  .name-date {
+    display: flex;
+    flex-direction: column;
+    margin-left: 10px;
+
+    span:nth-child(1) {
+      font-size: 15px;
+      font-weight: 500;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 1;
+    }
+
+    .time {
+      display: flex;
+      align-items: center;
+
+      span {
+        font-size: 12px;
+      }
+
+      img {
+        margin-left: 10px;
+      }
+
+      div {
+        border: 1.5px solid rgb(253, 81, 85);
+        border-radius: 8px;
+        line-height: 20px;
+        font-size: 12px;
+        transform: scale(0.7);
+        color: rgb(253, 81, 85);
+        padding: 0 4px;
+        background-color: rgba(250, 210, 211);
+
+        span {
+          font-weight: bolder;
+        }
+      }
+    }
+  }
+}
+
+.isExpire {
+  .name-date {
+    span:nth-child(1) {
+      color: rgba(0, 0, 0, 0.3);
+    }
+
+    .time {
+      div {
+        span {
+          font-weight: bolder;
+          color: red;
+        }
+      }
+    }
+  }
+}
+
+.table-url-box {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  span {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 1;
+    color: rgba(0, 0, 0, 0.4);
+  }
+}
+
+.times-box {
+  display: flex;
+  flex-direction: column;
+
+  .today-box {
+    span {
+      font-size: 13px;
+      font-weight: 600;
+      margin-right: 10px;
+    }
+
+    span:nth-child(1) {
+      font-weight: 400;
+      color: rgba(0, 0, 0, 0.4);
+    }
+  }
+
+  .total-box {
+    span {
+      font-size: 13px;
+      font-weight: 400;
+      margin-right: 10px;
+    }
+
+    span:nth-child(1) {
+      font-weight: 400;
+      color: rgba(0, 0, 0, 0.4);
+    }
+  }
+}
+
+.copy-url {
+  margin-left: 10px;
+}
+
+.demo-tabs > .el-tabs__content {
+  font-size: 32px;
+  font-weight: 600;
+}
+
+.demo-tabs .custom-tabs-label .el-icon {
+  vertical-align: middle;
+}
+
+.demo-tabs .custom-tabs-label span {
+  vertical-align: middle;
+  margin-left: 4px;
+}
+</style>
