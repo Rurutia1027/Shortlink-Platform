6fc0db831b5ad64690d2a5d75ccd4ead88f516b3
Rurutia1027 <rurutia1027@gmail.com>
2025-08-10 17:30:12 +0800

    feat: impl entry point files

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,26 @@
-import { createApp } from 'vue'
-import './style.css'
+import {createApp} from 'vue'
 import App from './App.vue'
+// import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+// import API from '@/api/index'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+// global mounted API
+// app.config.globalProperties.$API = API
+
+// close all Vue warning info
+app.config.warnHandler = () => null
+
+// app.use(router) // comment router for temporary
+
+app.use(ElementPlus)
+
+// register global ElementPlus icons
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+
+app.mount('#app')
