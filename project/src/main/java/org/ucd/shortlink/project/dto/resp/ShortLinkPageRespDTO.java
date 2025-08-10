25e9d0d41a6a568819daaab9d0daa8fd7bf141a6
Rurutia1027 <rurutia1027@gmail.com>
2025-08-10 12:50:30 +0800

    opt: add comment for each of the src code file

--- project/src/main/java/org/ucd/shortlink/project/dto/resp/ShortLinkPageRespDTO.java
+++ project/src/main/java/org/ucd/shortlink/project/dto/resp/ShortLinkPageRespDTO.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.ucd.shortlink.project.dto.resp;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -44,7 +61,7 @@ public class ShortLinkPageRespDTO {
     /**
      * Today pv cnt
      */
-    private Integer toDayPv;
+    private Integer todayPv;
 
     /**
      * History uv cnt
@@ -54,15 +71,15 @@ public class ShortLinkPageRespDTO {
     /**
      * Today uv cnt
      */
-    private Integer toDayUv;
+    private Integer todayUv;
 
     /**
      * History total uip
      */
-    private Integer totalUIp;
+    private Integer totalUip;
 
     /**
      * Today total uip
      */
-    private Integer toDayUIp;
+    private Integer todayUIp;
 }
--- project/src/main/java/org/ucd/shortlink/project/service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/org/ucd/shortlink/project/service/impl/ShortLinkServiceImpl.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.ucd.shortlink.project.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
@@ -163,13 +180,7 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
 
     @Override
     public IPage<ShortLinkPageRespDTO> pageShortLink(ShortLinkPageReqDTO requestParam) {
-        LambdaQueryWrapper<ShortLinkDO> queryWrapper = Wrappers.lambdaQuery(ShortLinkDO.class)
-                .eq(ShortLinkDO::getGid, requestParam.getGid())
-                .eq(ShortLinkDO::getEnableStatus, 0)
-                .eq(ShortLinkDO::getDelFlag, 0)
-                .orderByDesc(ShortLinkDO::getCreateTime);
-
-        IPage<ShortLinkDO> resultPage = baseMapper.selectPage(requestParam, queryWrapper);
+        IPage<ShortLinkDO> resultPage = baseMapper.pageLink(requestParam);
         return resultPage.convert(each -> {
             ShortLinkPageRespDTO result = BeanUtil.toBean(each, ShortLinkPageRespDTO.class);
             result.setDomain("http://" + result.getDomain());
