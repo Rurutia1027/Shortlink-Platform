ed3cfe731d74d7aa5bc1be2fcf8c5fdb3b4008fe
Rurutia1027 <rurutia1027@gmail.com>
2025-08-17 22:54:32 +0800

    test: establish controller TDD test case

--- project/src/test/java/org/ucd/shortlink/project/controller/RecycleBinControllerTest.java
+++ project/src/test/java/org/ucd/shortlink/project/controller/RecycleBinControllerTest.java
@@ -1,7 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.ucd.shortlink.project.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.baomidou.mybatisplus.test.autoconfigure.AutoConfigureMybatisPlus;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.ucd.shortlink.project.service.RecycleBinService;
 
+@WebMvcTest(controllers = RecycleBinController.class)
+@AutoConfigureMybatisPlus
 public class RecycleBinControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private RecycleBinService recycleBinService;
+
+    @Test
+    @DisplayName("Validate Mock Init OK")
+    public void testGetEmptyRecycleBin() {
+        Assertions.assertNotNull(mockMvc);
+        Assertions.assertNotNull(recycleBinService);
+    }
 
 }
